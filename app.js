b778d4a1ef0d0bc19c2f9f002e9fbc528adc77a9
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-22 18:40:44 -0300

    Rotas put e delete

--- app.js
+++ app.js
@@ -18,6 +18,34 @@ app.post('/products', (req, res) => {
     res.status(201).json(productsList);
 });
 
+app.put('/products/:name', (req, res) => {
+    const {name} = req.params;
+    const productInfo = req.body;
 
+    const findProduct = productsList.find(product => product.name === name);
+
+    if(!findProduct) {
+        return res.status(404).send({ message: 'Produto não encontrado' });  
+    }
+
+    productsList = productsList.map(product => product.name === name ? findProduct : productInfo);
+
+    res.status(200).json(productsList);
+
+});
+
+app.delete('/products/:name', (req, res) => {
+    const {name} = req.params;
+
+    const index = productsList.findIndex(product => product.name === name);
+
+    if(!index) {
+        return res.status(404).send({ message: 'Produto não encontrado' });  
+    }
+
+    const newProductsList = productsList.splice(index, 1);
+    console.log(newProductsList);
+    res.json(productsList);
+});
 
 app.listen(3000, () => console.log('Server online'));
