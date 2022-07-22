323b1b9a7a1e23ddf8b7453982bd474cd2450c27
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-22 18:35:57 -0300

    Projeto iniciado

---
+++ productsList.js
@@ -0,0 +1,10 @@
+let products = [
+    { name: 'Mouse', price: 20, quantity: 5, colors: ['white', 'black', 'blue'] },
+    { name: 'Tablet', price: 500, quantity: 20, colors: ['white', 'black'] },
+    { name: 'USB adaptor', price: 5, quantity: 0, colors: [] },
+    { name: 'Keyboard', price: 30, quantity: 35, colors: ['white'] },
+    { name: 'Gamepad', price: 30, quantity: 25, colors: ['black', 'silver'] },
+    { name: 'Monitor', price: 200, quantity: 3, colors: [] },
+    ]
+
+    module.exports = products;
