1089ffb396f1a62d034c788517493015318c65a9
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-22 18:40:19 -0300

    Rotas get e post

--- app.js
+++ app.js
@@ -6,5 +6,18 @@ let productsList = require('./productsList');
 app.use(express.json());
 
 
+app.get('/products', (req, res) => {
+    console.log(productsList);
+    res.status(200).json(productsList);
+});
+
+app.post('/products', (req, res) => {
+    const product = req.body;
+    productsList = [...productsList, product];
+
+    res.status(201).json(productsList);
+});
+
+
 
 app.listen(3000, () => console.log('Server online'));
